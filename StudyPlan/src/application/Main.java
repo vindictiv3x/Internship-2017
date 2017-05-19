2d4f42c30ca0970f00373e28b377fbb9f9462afb
MaksimEkin <meren@montgomerycollege.edu>
2017-05-18 22:08:13 -0400

    Added the study plan table view

--- StudyPlan/src/application/Main.java
+++ StudyPlan/src/application/Main.java
@@ -5,42 +5,15 @@ package application;
 
 //The import files needed for the project 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.stage.Stage;
+
  
 
 /**
@@ -55,74 +28,46 @@ public class Main extends Application {
 		launch(args);
 	}
 	
-	//BUTTONS NEEDED
-	static Button exit = new Button("Exit");
-	
-	//RADIO BUTTONS NEEDED
-	static RadioButton readStudyRB = new RadioButton("Read Study Plan");
-	static RadioButton newStudyRB = new RadioButton("Create New Study Plan");
-	
 	//Table
-	public TableView table = new TableView();
+	public TableView<StudyPlanDriver> table;
+	
+	
+	
+	@Override
+	public void init(){
+		table = new TableView<StudyPlanDriver>();
+		
+		table.getColumns().addAll(StudyPlanDriver.getColumn(table));
+		
+		table.setItems(getStudyPlanDummy());
+		
+		table.setOnMouseClicked(new EventHandler<MouseEvent>(){
+
+			@Override
+			public void handle(MouseEvent event) {
+				if(event.getClickCount()> 0){
+					if(table.getSelectionModel().getSelectedIndex() >= 0){
+						System.out.println(table.getSelectionModel().getSelectedItem().getBook());
+					}
+				}
+				
+			}
+			
+		});
+		
+		
+		
+	}
 	
 	
 	//The GUI
 	public void start(Stage primaryStage) {
 		
-		
-		
-		//Organize the radio buttons 
-		HBox radioButtons1 = new HBox(20);
-		//Create toggle group for the radio buttons
-		ToggleGroup group = new ToggleGroup();
-		readStudyRB.setToggleGroup(group);
-		newStudyRB.setToggleGroup(group);
-		//Add the radio buttons to the hbox
-		radioButtons1.getChildren().addAll(readStudyRB, newStudyRB);
-
-		//Set Location of the Radio Buttons
-		radioButtons1.setLayoutX(900);
-		radioButtons1.setLayoutY(100);	
-		
-		//Set the Location of the exit button
-		exit.setLayoutX(2050);
-		exit.setLayoutY(1100);
-		
-		//Set the header to the gui 
-		final Label studyPlanlabel = new Label("Study Plan");
-		studyPlanlabel.setFont(new Font("Arial" , 20));
-		
-		//Set the table editable
-		table.setEditable(true);
-		
-		//CREATE TABLE COLUMNS
-		TableColumn examObjectivesCol = new TableColumn("Exam Objectives");
-		TableColumn bookChapterCol = new TableColumn("Book Chapter");
-		TableColumn dateCol = new TableColumn("Date");
-		TableColumn notesCol = new TableColumn("Notes");
-		
-		
-		//Add the columns to the table
-		table.getColumns().addAll(examObjectivesCol, bookChapterCol, dateCol, notesCol);
-		
-		//Set the size and the location of the table 
-		table.setLayoutX(600);
-		table.setLayoutY(160);
-		table.setPrefSize(1000, 900);
-		
-		
-		
-		//Exit when the exit button is pressed
-		exit.addEventFilter(MouseEvent.MOUSE_CLICKED,
-				new EventHandler<MouseEvent>(){
-			public void handle(MouseEvent event){
-				System.exit(0);
-				return;
-			}
-		});
+		table.setPrefSize(1250, 500);
+	
 		
 		//Creating the Group Object 
-		Group root = new Group(exit, radioButtons1, studyPlanlabel,table);
+		Group root = new Group(table);
 		
 		//Creating a scene object
 		Scene scene = new Scene(root);
@@ -143,5 +88,30 @@ public class Main extends Application {
 		
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
+	public static ObservableList<StudyPlanDriver> getStudyPlanDummy(){
+		ObservableList<StudyPlanDriver> data = FXCollections.observableArrayList();
+		
+		data.addAll(new StudyPlanDriver("", "1", "", ""));
+		data.addAll(new StudyPlanDriver("", "Review questions", "", ""));
+		data.addAll(new StudyPlanDriver("", "2", "", ""));
+		data.addAll(new StudyPlanDriver("", "Review questions", "", ""));
+		data.addAll(new StudyPlanDriver("", "3", "", ""));
+		data.addAll(new StudyPlanDriver("", "Review questions", "", ""));
+		data.addAll(new StudyPlanDriver("", "4", "", ""));
+		data.addAll(new StudyPlanDriver("", "Review questions", "", ""));
+		data.addAll(new StudyPlanDriver("", "5", "", ""));
+		data.addAll(new StudyPlanDriver("", "Review questions", "", ""));
+		data.addAll(new StudyPlanDriver("", "6", "", ""));
+		data.addAll(new StudyPlanDriver("", "Review questions", "", ""));
+		data.addAll(new StudyPlanDriver("", "Take Free Oracle\nPractice Exam", "", ""));
+		data.addAll(new StudyPlanDriver("", "Take Practice Exam\navailable in the book\nused to study", "", ""));
+		data.addAll(new StudyPlanDriver("", "Use flashcards to \npractice", "", ""));
+		data.addAll(new StudyPlanDriver("", "Take Test", "", ""));
+		return data;
+	}
 
 }
--- StudyPlan/src/application/StudyPlanDriver.java
+++ StudyPlan/src/application/StudyPlanDriver.java
@@ -3,6 +3,10 @@ package application;
 import java.io.File;
 import java.util.ArrayList;
 
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 /**
  * 
  * @author Maksim
@@ -12,26 +16,102 @@ public class StudyPlanDriver implements StudyPlanDriverInterface {
 
 	//VARIABLES NEEDED 
 	String examObjectives;
-	String bookChapter;
+	String book;
 	String date;
 	String notes; 
 	
 	
 	/**
-	 * Empty Constructor 
+	 * Constractor
 	 */
-	public StudyPlanDriver(String examObjectives, String bookChapter, String date
+	public StudyPlanDriver(String examObjectives, String book, String date
 			, String notes){
 		
 		this.examObjectives = examObjectives;
-		this.bookChapter = bookChapter;
+		this.book = book;
 		this.date = date;
 		this.notes= notes;
 		
 	}
 	
+	/**
+	 * Empty Constractor 
+	 */
+	public StudyPlanDriver(){
+		this.examObjectives ="";
+		this.book = "";
+		this.date = "";
+		this.notes = "";
+	}
 	
 	
+	
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getExamObjectives() {
+		return examObjectives;
+	}
+
+	/**
+	 * 
+	 * @param examObjectives
+	 */
+	public void setExamObjectives(String examObjectives) {
+		this.examObjectives = examObjectives;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String getDate() {
+		return date;
+	}
+
+	/**
+	 * 
+	 * @param date
+	 */
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String getNotes() {
+		return notes;
+	}
+
+	/**
+	 * 
+	 * @param notes
+	 */
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getBook(){
+		return book;
+	}
+	
+	/**
+	 * 
+	 * @param book
+	 */
+	public void setBook(String book){
+		this.book = book;
+	}
+	
+
 	/**
 	 * Reads Study Plan
 	 * @param file the file to read
@@ -57,6 +137,48 @@ public class StudyPlanDriver implements StudyPlanDriverInterface {
 	public String toString(){
 		
 		return"";
+	}
+	
+	/**
+	 * 
+	 * @param table
+	 * @return
+	 */
+	public static ArrayList<TableColumn<StudyPlanDriver,?>> getColumn(TableView table){
+		
+		int i;
+		ArrayList<TableColumn<StudyPlanDriver, ?>> columns = new ArrayList<TableColumn<StudyPlanDriver,?>>();
+		
+		String[] columnNames = {"Exam Objectives", "Book Chapter", "Date", "Notes"};
+		String[] variableNames = {"examObjectives", "book", "date", "notes"};
+		Integer[] column_width = {25, 25, 25, 25};
+		
+		i = 0;
+		TableColumn<StudyPlanDriver, String> examObjectivesCol = new TableColumn<>(columnNames[i++]); 
+		TableColumn<StudyPlanDriver, String> bookCol = new TableColumn<>(columnNames[i++]); 
+		TableColumn<StudyPlanDriver, String> dateCol = new TableColumn<>(columnNames[i++]); 
+		TableColumn<StudyPlanDriver, String> notesCol = new TableColumn<>(columnNames[i++]); 
+		
+		i=0;
+		examObjectivesCol.prefWidthProperty().bind(table.widthProperty().divide(100/column_width[i++]));
+		bookCol.prefWidthProperty().bind(table.widthProperty().divide(100/column_width[i++]));
+		dateCol.prefWidthProperty().bind(table.widthProperty().divide(100/column_width[i++]));
+		notesCol.prefWidthProperty().bind(table.widthProperty().divide(100/column_width[i++]));
+		
+		i =0;
+		examObjectivesCol.setCellValueFactory(new PropertyValueFactory<StudyPlanDriver, String>(variableNames[i++]));
+		bookCol.setCellValueFactory(new PropertyValueFactory<StudyPlanDriver, String>(variableNames[i++]));
+		dateCol.setCellValueFactory(new PropertyValueFactory<StudyPlanDriver, String>(variableNames[i++]));
+		notesCol.setCellValueFactory(new PropertyValueFactory<StudyPlanDriver, String>(variableNames[i++]));
+		
+		
+		columns.add(examObjectivesCol);
+		columns.add(bookCol);
+		columns.add(dateCol);
+		columns.add(notesCol);
+		
+		return  columns;
+		
 	}
 	
 
